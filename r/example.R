f5c194132bfe014d7e3146a6cd863710cd307a5e
Lee <David.Lee@defra.gov.uk>
2020-05-13 08:35:34 +0100

    push test2

--- r/example.R
+++ r/example.R
@@ -2,8 +2,8 @@ pacman::p_load(tidyverse, tidyxl, unpivotr, here)
 
 source(here("r", "get_product_tables.R"))
 
-cells <- xlsx_cells(here("data", "kantar_product.xlsx"))
-formats <- xlsx_formats(here("data", "kantar_product.xlsx"))
+cells <- xlsx_cells(here("data", "raw", "kantar_product.xlsx"))
+formats <- xlsx_formats(here("data", "raw", "kantar_product.xlsx"))
 indent <- formats$local$alignment$indent
 
 
